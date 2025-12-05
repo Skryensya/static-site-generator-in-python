d4d9843f7859ab4f510eef90d267387ece39d143
Allison Peña <allison@asimov.cl>
2025-12-05 17:10:07 -0300

    messy progress

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -16,20 +16,27 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             
             instances = math.ceil(len(splitted) / 3)
 
-         
             first_split = node.text.split(delimiter, 1)
             prev = first_split[0]
             second_split = first_split[1].split(delimiter, 1)
             actual_block = second_split[0]
-            next_nodes = []
+            rest_node = TextNode(second_split[1], TextType.TEXT)
+            next_nodes = []    
             if(instances == 1):
-                next_nodes.append(TextNode(second_split[1], TextType.TEXT))
+                next_nodes.append(rest_node)
             else:
-                next_nodes = split_nodes_delimiter(second_split[1])
+                next_nodes = split_nodes_delimiter([rest_node], delimiter, text_type)
 
             new_nodes.extend([
                 TextNode(prev, TextType.TEXT),
-                TextNode(actual_block, TextType.CODE), 
+                TextNode(actual_block, text_type), 
             ])
-            next_nodes.extend(next_nodes)
+            new_nodes.extend(next_nodes)
+
+            ni = 0
+            # filter "" text nodes
+            for node  in new_nodes:
+                if (node == TextNode("", TextType.TEXT)):
+                    new_nodes.pop(ni) 
+                ni +=1 
     return new_nodes
--- src/tests/test_split_nodes_delimeter.py
+++ src/tests/test_split_nodes_delimeter.py
@@ -6,33 +6,46 @@ from src.split_nodes_delimiter import split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_code_block(self):
-        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        node = TextNode("A `B` C", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(new_nodes, [
-            TextNode("This is text with a ", TextType.TEXT),
-            TextNode("code block", TextType.CODE),
-            TextNode(" word", TextType.TEXT),
+            TextNode("A ", TextType.TEXT),
+            TextNode("B", TextType.CODE),
+            TextNode(" C", TextType.TEXT),
         ])
 
-    # def test_code_block_2(self):
-    #     node = TextNode("This is text with a `code block` word and another `code block` lol", TextType.TEXT)
-    #     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    #     self.assertEqual(new_nodes, [
-    #         TextNode("This is text with a ", TextType.TEXT),
-    #         TextNode("code block", TextType.CODE),
-    #         TextNode(" word and another ", TextType.TEXT),
-    #         TextNode("code block", TextType.CODE),
-    #         TextNode(" lol", TextType.TEXT),
-    #     ])
-    # def test_code_block_3(self):
-    #     node = TextNode("`code block` word and another `code block` `code block`", TextType.TEXT)
-    #     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    #     self.assertEqual(new_nodes, [
-    #         TextNode("This is text with a ", TextType.TEXT),
-    #         TextNode("code block", TextType.CODE),
-    #         TextNode(" word", TextType.TEXT),
-    #     ])
+    def test_code_block_2(self):
+        node = TextNode("A `B` C `D` E", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("A ", TextType.TEXT),
+            TextNode("B", TextType.CODE),
+            TextNode(" C ", TextType.TEXT),
+            TextNode("D", TextType.CODE),
+            TextNode(" E", TextType.TEXT),
+        ])
+        
+    def test_code_block_3(self):
+        node = TextNode("`A` B `C` `D`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("A", TextType.CODE),
+            TextNode(" B ", TextType.TEXT),
+            TextNode("C", TextType.CODE),
+            TextNode(" ", TextType.TEXT),
+            TextNode("D", TextType.CODE)
+        ])
 
+    def test_italic(self):
+        node = TextNode("**A** B **C** **D**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("A", TextType.BOLD),
+            TextNode(" B ", TextType.TEXT),
+            TextNode("C", TextType.BOLD),
+            TextNode(" ", TextType.TEXT),
+            TextNode("D", TextType.BOLD)
+        ])
 
 if __name__ == "__main__":
     unittest.main()
